6a92a2bf744fa4820dfb1b2c35d7f46da56ce2a5
Nicolaj123411 <nicolaj123411@gmail.com>
2017-10-31 20:57:26 +0100

    ikke mange ændringer men dato er ved at være på plads

--- pages/events.php
+++ pages/events.php
@@ -7,7 +7,19 @@
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus, odit iure veritatis eaque nulla dignissimos eius ut, tempora dicta facilis.</p>
 		</div>
 		<div class="event-image">
-			<img src="http://via.placeholder.com/150x150" alt="Event picture">
+
+			<img src="http://via.placeholder.com/250x250" alt="Event picture">
+			<div class="img-overlay">
+				<p>Starter om</p>
+				<div>
+					<p>10</p>
+					<p>timer</p>
+				</div>
+				<div>
+					<p>20</p>
+					<p>min</p>
+				</div>
+			</div>
 		</div>
 		</div>
 		
@@ -15,5 +27,7 @@
 	<div class="accordion-content">
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam sit eligendi repellendus et quibusdam ipsa dolorum doloribus, ipsam hic quae tempora, est expedita mollitia nihil a laboriosam, ipsum facilis saepe voluptas illo eaque! Doloremque ipsum, odit, fugit ipsam amet soluta dolor cupiditate sint, veritatis alias facilis quod accusamus in id!</p>
 	</div>
+
+
 
 </div>
