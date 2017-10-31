c02fce1a170f3aaba10898b149d0f982105f2b4c
Nicolaj123411 <nicolaj123411@gmail.com>
2017-10-31 20:09:36 +0100

    tilføjet indhold på event side

--- pages/events.php
+++ pages/events.php
@@ -10,4 +10,40 @@
 	<div class="accordion-content">
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam sit eligendi repellendus et quibusdam ipsa dolorum doloribus, ipsam hic quae tempora, est expedita mollitia nihil a laboriosam, ipsum facilis saepe voluptas illo eaque! Doloremque ipsum, odit, fugit ipsam amet soluta dolor cupiditate sint, veritatis alias facilis quod accusamus in id!</p>
 	</div>
+
+		<button class="accordion flex">
+		<div class="event-desc">
+			<h1>OVERSKRIFT</h1>
+			<h3>UNDERSKRFIT</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus, odit iure veritatis eaque nulla dignissimos eius ut, tempora dicta facilis.</p>
+		</div>
+		<img src="http://via.placeholder.com/350x150" alt="Event picture">
+	</button>
+	<div class="accordion-content">
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam sit eligendi repellendus et quibusdam ipsa dolorum doloribus, ipsam hic quae tempora, est expedita mollitia nihil a laboriosam, ipsum facilis saepe voluptas illo eaque! Doloremque ipsum, odit, fugit ipsam amet soluta dolor cupiditate sint, veritatis alias facilis quod accusamus in id!</p>
+	</div>
+
+		<button class="accordion flex">
+		<div class="event-desc">
+			<h1>OVERSKRIFT</h1>
+			<h3>UNDERSKRFIT</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus, odit iure veritatis eaque nulla dignissimos eius ut, tempora dicta facilis.</p>
+		</div>
+		<img src="http://via.placeholder.com/350x150" alt="Event picture">
+	</button>
+	<div class="accordion-content">
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam sit eligendi repellendus et quibusdam ipsa dolorum doloribus, ipsam hic quae tempora, est expedita mollitia nihil a laboriosam, ipsum facilis saepe voluptas illo eaque! Doloremque ipsum, odit, fugit ipsam amet soluta dolor cupiditate sint, veritatis alias facilis quod accusamus in id!</p>
+	</div>
+
+		<button class="accordion flex">
+		<div class="event-desc">
+			<h1>OVERSKRIFT</h1>
+			<h3>UNDERSKRFIT</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus, odit iure veritatis eaque nulla dignissimos eius ut, tempora dicta facilis.</p>
+		</div>
+		<img src="http://via.placeholder.com/350x150" alt="Event picture">
+	</button>
+	<div class="accordion-content">
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam sit eligendi repellendus et quibusdam ipsa dolorum doloribus, ipsam hic quae tempora, est expedita mollitia nihil a laboriosam, ipsum facilis saepe voluptas illo eaque! Doloremque ipsum, odit, fugit ipsam amet soluta dolor cupiditate sint, veritatis alias facilis quod accusamus in id!</p>
+	</div>
 </div>
