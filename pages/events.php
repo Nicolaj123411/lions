fddd05235eee9b1214a74755e226879621900261
Nicolaj123411 <nicolaj123411@gmail.com>
2017-11-01 13:39:46 +0100

    bla

--- pages/events.php
+++ pages/events.php
@@ -1,45 +1,6 @@
 <div id="wrapper">
 	<section id="main_content">
 <div class="event">
-	<button class="accordion">
-		<div class="flex">
-		<div class="event-desc">
-			<h1>NÆSTE EVENT</h1>
-			<h3>25/9 Loppemarked</h3>
-			<p>Det er utroligt vigtigt med hurtig hjælp, da tusindvis af børn i Caribien lige nu står med et akut behov for vand, mad og beskyttelse efter orkanen Irmas voldsomme hærgen. Lions Katastrofehjælp har derfor straks doneret 250.000 kroner til UNICEFs indsats i regionen.</p>
-		<ul class="info_icon">
-			<i class="fa fa-wheelchair"></i>
-			<i class="fa fa-coffee"></i>
-			<i class="fa fa-eur"></i>
-			<i class="fa fa-blind"></i>
-		</ul>
-		</div>
-		<div class="event-image">
-			<img src="http://via.placeholder.com/500x400" alt="Event picture">
-				<div class="img-overlay">
-					<p>Starter om</p>
-					<div>
-						<p>10</p>
-						<p>timer</p>
-					</div>
-					<div>
-						<p>20</p>
-						<p>min</p>
-					</div>
-				</div>
-			</div>
-		</div>
-		
-	</button>
-	<div class="accordion-content">
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam sit eligendi repellendus et quibusdam ipsa dolorum doloribus, ipsam hic quae tempora, est expedita mollitia nihil a laboriosam, ipsum facilis saepe voluptas illo eaque! Doloremque ipsum, odit, fugit ipsam amet soluta dolor cupiditate sint, veritatis alias facilis quod accusamus in id!</p>
-	
-		<p>Lorem ipsum dolor sit amet,s consectetur adipisicing elit. Totam sit eligendi repellendus et quibusdam ipsa dolorum doloribus, ipsam hic quae tempora, est expedita mollitia nihil a laboriosam, ipsum facilis saepe voluptas illo eaque! Doloremque ipsum, odit, fugit ipsam amet soluta dolor cupiditate sint, veritatis alias facilis quod accusamus in id!</p>
-		<p>Lorem ipsum dolor sit amet,s consectetur adipisicing elit. Totam sit eligendi repellendus et quibusdam ipsa dolorum doloribus, ipsam hic quae tempora, est expedita mollitia nihil a laboriosam, ipsum facilis saepe voluptas illo eaque! Doloremque ipsum, odit, fugit ipsam amet soluta dolor cupiditate sint, veritatis alias facilis quod accusamus in id!</p>
-
-
-		<button>luk event</button>
-</div>
 
 	<button class="accordion">
 		<div class="flex">
@@ -72,6 +33,7 @@
 		
 	</button>
 	<div class="accordion-content">
+		<div class="event_desc_wrapper">jeg er afstand
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam sit eligendi repellendus et quibusdam ipsa dolorum doloribus, ipsam hic quae tempora, est expedita mollitia nihil a laboriosam, ipsum facilis saepe voluptas illo eaque! Doloremque ipsum, odit, fugit ipsam amet soluta dolor cupiditate sint, veritatis alias facilis quod accusamus in id!</p>
 	
 		<p>Lorem ipsum dolor sit amet,s consectetur adipisicing elit. Totam sit eligendi repellendus et quibusdam ipsa dolorum doloribus, ipsam hic quae tempora, est expedita mollitia nihil a laboriosam, ipsum facilis saepe voluptas illo eaque! Doloremque ipsum, odit, fugit ipsam amet soluta dolor cupiditate sint, veritatis alias facilis quod accusamus in id!</p>
@@ -79,127 +41,10 @@
 
 
 		<button>luk event</button>
+		</div>
 </div>
 
-	<button class="accordion">
-		<div class="flex">
-		<div class="event-desc">
-			<h1>NÆSTE EVENT</h1>
-			<h3>25/9 Loppemarked</h3>
-			<p>Det er utroligt vigtigt med hurtig hjælp, da tusindvis af børn i Caribien lige nu står med et akut behov for vand, mad og beskyttelse efter orkanen Irmas voldsomme hærgen. Lions Katastrofehjælp har derfor straks doneret 250.000 kroner til UNICEFs indsats i regionen.</p>
-		<ul class="info_icon">
-			<i class="fa fa-wheelchair"></i>
-			<i class="fa fa-coffee"></i>
-			<i class="fa fa-eur"></i>
-			<i class="fa fa-blind"></i>
-		</ul>
-		</div>
-		<div class="event-image">
-			<img src="http://via.placeholder.com/500x400" alt="Event picture">
-				<div class="img-overlay">
-					<p>Starter om</p>
-					<div>
-						<p>10</p>
-						<p>timer</p>
-					</div>
-					<div>
-						<p>20</p>
-						<p>min</p>
-					</div>
-				</div>
-			</div>
-		</div>
-		
-	</button>
-	<div class="accordion-content">
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam sit eligendi repellendus et quibusdam ipsa dolorum doloribus, ipsam hic quae tempora, est expedita mollitia nihil a laboriosam, ipsum facilis saepe voluptas illo eaque! Doloremque ipsum, odit, fugit ipsam amet soluta dolor cupiditate sint, veritatis alias facilis quod accusamus in id!</p>
-	
-		<p>Lorem ipsum dolor sit amet,s consectetur adipisicing elit. Totam sit eligendi repellendus et quibusdam ipsa dolorum doloribus, ipsam hic quae tempora, est expedita mollitia nihil a laboriosam, ipsum facilis saepe voluptas illo eaque! Doloremque ipsum, odit, fugit ipsam amet soluta dolor cupiditate sint, veritatis alias facilis quod accusamus in id!</p>
-		<p>Lorem ipsum dolor sit amet,s consectetur adipisicing elit. Totam sit eligendi repellendus et quibusdam ipsa dolorum doloribus, ipsam hic quae tempora, est expedita mollitia nihil a laboriosam, ipsum facilis saepe voluptas illo eaque! Doloremque ipsum, odit, fugit ipsam amet soluta dolor cupiditate sint, veritatis alias facilis quod accusamus in id!</p>
 
-
-		<button>luk event</button>
-</div>
-
-	<button class="accordion">
-		<div class="flex">
-		<div class="event-desc">
-			<h1>NÆSTE EVENT</h1>
-			<h3>25/9 Loppemarked</h3>
-			<p>Det er utroligt vigtigt med hurtig hjælp, da tusindvis af børn i Caribien lige nu står med et akut behov for vand, mad og beskyttelse efter orkanen Irmas voldsomme hærgen. Lions Katastrofehjælp har derfor straks doneret 250.000 kroner til UNICEFs indsats i regionen.</p>
-		<ul class="info_icon">
-			<i class="fa fa-wheelchair"></i>
-			<i class="fa fa-coffee"></i>
-			<i class="fa fa-eur"></i>
-			<i class="fa fa-blind"></i>
-		</ul>
-		</div>
-		<div class="event-image">
-			<img src="http://via.placeholder.com/500x400" alt="Event picture">
-				<div class="img-overlay">
-					<p>Starter om</p>
-					<div>
-						<p>10</p>
-						<p>timer</p>
-					</div>
-					<div>
-						<p>20</p>
-						<p>min</p>
-					</div>
-				</div>
-			</div>
-		</div>
-		
-	</button>
-	<div class="accordion-content">
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam sit eligendi repellendus et quibusdam ipsa dolorum doloribus, ipsam hic quae tempora, est expedita mollitia nihil a laboriosam, ipsum facilis saepe voluptas illo eaque! Doloremque ipsum, odit, fugit ipsam amet soluta dolor cupiditate sint, veritatis alias facilis quod accusamus in id!</p>
-	
-		<p>Lorem ipsum dolor sit amet,s consectetur adipisicing elit. Totam sit eligendi repellendus et quibusdam ipsa dolorum doloribus, ipsam hic quae tempora, est expedita mollitia nihil a laboriosam, ipsum facilis saepe voluptas illo eaque! Doloremque ipsum, odit, fugit ipsam amet soluta dolor cupiditate sint, veritatis alias facilis quod accusamus in id!</p>
-		<p>Lorem ipsum dolor sit amet,s consectetur adipisicing elit. Totam sit eligendi repellendus et quibusdam ipsa dolorum doloribus, ipsam hic quae tempora, est expedita mollitia nihil a laboriosam, ipsum facilis saepe voluptas illo eaque! Doloremque ipsum, odit, fugit ipsam amet soluta dolor cupiditate sint, veritatis alias facilis quod accusamus in id!</p>
-
-
-		<button>luk event</button>
-</div>
-
-	<button class="accordion">
-		<div class="flex">
-		<div class="event-desc">
-			<h1>NÆSTE EVENT</h1>
-			<h3>25/9 Loppemarked</h3>
-			<p>Det er utroligt vigtigt med hurtig hjælp, da tusindvis af børn i Caribien lige nu står med et akut behov for vand, mad og beskyttelse efter orkanen Irmas voldsomme hærgen. Lions Katastrofehjælp har derfor straks doneret 250.000 kroner til UNICEFs indsats i regionen.</p>
-		<ul class="info_icon">
-			<i class="fa fa-wheelchair"></i>
-			<i class="fa fa-coffee"></i>
-			<i class="fa fa-eur"></i>
-			<i class="fa fa-blind"></i>
-		</ul>
-		</div>
-		<div class="event-image">
-			<img src="http://via.placeholder.com/500x400" alt="Event picture">
-				<div class="img-overlay">
-					<p>Starter om</p>
-					<div>
-						<p>10</p>
-						<p>timer</p>
-					</div>
-					<div>
-						<p>20</p>
-						<p>min</p>
-					</div>
-				</div>
-			</div>
-		</div>
-		
-	</button>
-	<div class="accordion-content">
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam sit eligendi repellendus et quibusdam ipsa dolorum doloribus, ipsam hic quae tempora, est expedita mollitia nihil a laboriosam, ipsum facilis saepe voluptas illo eaque! Doloremque ipsum, odit, fugit ipsam amet soluta dolor cupiditate sint, veritatis alias facilis quod accusamus in id!</p>
-	
-		<p>Lorem ipsum dolor sit amet,s consectetur adipisicing elit. Totam sit eligendi repellendus et quibusdam ipsa dolorum doloribus, ipsam hic quae tempora, est expedita mollitia nihil a laboriosam, ipsum facilis saepe voluptas illo eaque! Doloremque ipsum, odit, fugit ipsam amet soluta dolor cupiditate sint, veritatis alias facilis quod accusamus in id!</p>
-		<p>Lorem ipsum dolor sit amet,s consectetur adipisicing elit. Totam sit eligendi repellendus et quibusdam ipsa dolorum doloribus, ipsam hic quae tempora, est expedita mollitia nihil a laboriosam, ipsum facilis saepe voluptas illo eaque! Doloremque ipsum, odit, fugit ipsam amet soluta dolor cupiditate sint, veritatis alias facilis quod accusamus in id!</p>
-
-
-		<button>luk event</button>
-</div>
 
 
 
