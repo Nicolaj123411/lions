dc19b0370287395e1c46fea170f6577bbfaa8c51
Nicolaj123411 <nicolaj123411@gmail.com>
2017-11-01 06:11:59 +0100

    test af branch checkout npr der er lavet ændringer

--- pages/events.php
+++ pages/events.php
@@ -9,5 +9,6 @@
 	</button>
 	<div class="accordion-content">
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam sit eligendi repellendus et quibusdam ipsa dolorum doloribus, ipsam hic quae tempora, est expedita mollitia nihil a laboriosam, ipsum facilis saepe voluptas illo eaque! Doloremque ipsum, odit, fugit ipsam amet soluta dolor cupiditate sint, veritatis alias facilis quod accusamus in id!</p>
+		<button>luk event</button>
 	</div>
 </div>
