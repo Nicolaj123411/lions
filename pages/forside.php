ba3d7a043a98845b272afdc323b6d5be30907f1d
Mathias <mathias@Mathiass-MacBook-Pro.local>
2017-11-01 11:07:05 +0100

    forside 3 news cards

--- pages/forside.php
+++ pages/forside.php
@@ -1,13 +1,26 @@
-<ul>
-	<li><a href="">Lorem ipsum.</a></li>
-	<li><a href="">Accusantium, rem?</a></li>
-	<li><a href="">Necessitatibus, pariatur.</a></li>
-	<li><a href="">Optio, cupiditate.</a></li>
-	<li><a href="">Nulla, magni?</a></li>
-	<li><a href="">Debitis, odio!</a></li>
-	<li><a href="">Hic, cupiditate.</a></li>
-	<li><a href="">Quod, beatae!</a></li>
-	<li><a href="">At, tempore.</a></li>
-	<li><a href="">Consequatur, nostrum!</a></li>
-</ul>
+<div id="wrapper">
+<section id="main_content">
+	<section class="flex" id="news_card">
+		<article>
+			<img src="http://via.placeholder.com/350x250" alt="">
+			<p id="news_card_headline">Overskrift 1</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas eos quo repudiandae repellat exercitationem quam, aspernatur obcaecati nulla suscipit quod.</p>
+			<p id="news_card_bottomlink">LÆS MERE</p>
+		</article>
 
+		<article>
+			<img src="http://via.placeholder.com/350x250" alt="">
+			<p id="news_card_headline">Overskrift 2</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore fugiat dolor, voluptatum impedit natus amet similique ipsam dolore dignissimos quidem ad, laborum aliquam quia eos facere nam numquam saepe pariatur!</p>
+			<p id="news_card_bottomlink">LÆS MERE</p>
+		</article>
+
+		<article>
+			<img src="http://via.placeholder.com/350x250" alt="">
+			<p id="news_card_headline">Overskrift 3</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus facere quia debitis laborum, porro dolorum esse tempore! Quas ducimus, ipsa numquam explicabo eos nemo atque, repudiandae sequi, dolores consequatur enim.</p>
+			<p id="news_card_bottomlink">LÆS MERE</p>
+		</article>
+	</section>
+</section>
+</div>
