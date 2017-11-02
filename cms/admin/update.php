974df6708920d0eb4af45c5ba2640599664e3c56
Mathias <mathias@Mathiass-MacBook-Pro.local>
2017-11-02 10:49:19 +0100

    link

--- cms/admin/update.php
+++ cms/admin/update.php
@@ -3,7 +3,7 @@
 <head>
 	<title>ADMIN PANEL</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" href="/cms/assets/css/style.css">
+	<link rel="stylesheet" href="../assets/css/style.css">
 </head>
 <body>
 
