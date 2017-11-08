952e52e0d56a1638e80f04015643283764a26a8f
Nicolaj123411 <nicolaj123411@gmail.com>
2017-11-08 10:39:02 +0100

    accordian menu

--- includes/kalender.php
+++ includes/kalender.php
@@ -7,16 +7,103 @@
 
 
 		<div id="events_list_wrapper">
-			Events list wrapper
+			
 
 			<div id="events_list">
-				event
+							<button class="accordion">
+				<div class="flex">
+				<div class="event-desc">
+					<h3>25/9 Loppemarked</h3>
+					<p>Det er utroligt vigtigt med hurtig hjælp, da tusindvis af børn i Caribien lige nu står med et akut behov for vand, mad og beskyttelse efter orkanen Irmas voldsomme hærgen. Lions Katastrofehjælp har derfor straks doneret 250.000 kroner til UNICEFs indsats i regionen.</p>
+				</div>
+				</div>
+				
+				
+			</button>
+			<div class="accordion-content">
+				<div class="event_desc_wrapper">
+
+					<img style="float: right;" src="http://via.placeholder.com/200x200" alt="event description image">
+					<h5>lille overskrift</h5>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam sit eligendi repellendus et quibusdam ipsa dolorum doloribus, ipsam hic quae tempora, est expedita mollitia nihil a laboriosam, ipsum facilis saepe voluptas illo eaque! Doloremque ipsum, odit, fugit ipsam amet soluta dolor cupiditate sint, veritatis alias facilis quod accusamus in id!</p>
+				<h5>lille overskrift</h5> 
+				
+			
+				<p>Lorem ipsum dolor sit amet,s consectetur adipisicing elit. Totam sit eligendi repellendus et quibusdam ipsa dolorum doloribus, ipsam hic quae tempora, est expedita mollitia nihil a laboriosam, ipsum facilis saepe voluptas illo eaque! Doloremque ipsum, odit, fugit ipsam amet soluta dolor cupiditate sint, veritatis alias facilis quod accusamus in id!</p>
+		
+				<h5>Tidsplan</h5>
+				<div class="tidsplan">
+				<span>08:20 -  </span><p>Der er opvisning der og der </p>
+				<span>11:25 -  </span><p>Madboden åbner op for slag at mad og drikke huisk at medbring kontanter</p>
+				
+				</div>
+			
+				</div>
+		</div>
 			</div>
 			<div id="events_list">
-				event
+							<button class="accordion">
+				<div class="flex">
+				<div class="event-desc">
+					<h3>25/9 Loppemarked</h3>
+					<p>Det er utroligt vigtigt med hurtig hjælp, da tusindvis af børn i Caribien lige nu står med et akut behov for vand, mad og beskyttelse efter orkanen Irmas voldsomme hærgen. Lions Katastrofehjælp har derfor straks doneret 250.000 kroner til UNICEFs indsats i regionen.</p>
+				</div>
+				</div>
+				
+				
+			</button>
+			<div class="accordion-content">
+				<div class="event_desc_wrapper">
+
+					<img style="float: right;" src="http://via.placeholder.com/200x200" alt="event description image">
+					<h5>lille overskrift</h5>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam sit eligendi repellendus et quibusdam ipsa dolorum doloribus, ipsam hic quae tempora, est expedita mollitia nihil a laboriosam, ipsum facilis saepe voluptas illo eaque! Doloremque ipsum, odit, fugit ipsam amet soluta dolor cupiditate sint, veritatis alias facilis quod accusamus in id!</p>
+				<h5>lille overskrift</h5> 
+				
+			
+				<p>Lorem ipsum dolor sit amet,s consectetur adipisicing elit. Totam sit eligendi repellendus et quibusdam ipsa dolorum doloribus, ipsam hic quae tempora, est expedita mollitia nihil a laboriosam, ipsum facilis saepe voluptas illo eaque! Doloremque ipsum, odit, fugit ipsam amet soluta dolor cupiditate sint, veritatis alias facilis quod accusamus in id!</p>
+		
+				<h5>Tidsplan</h5>
+				<div class="tidsplan">
+				<span>08:20 -  </span><p>Der er opvisning der og der </p>
+				<span>11:25 -  </span><p>Madboden åbner op for slag at mad og drikke huisk at medbring kontanter</p>
+				
+				</div>
+			
+				</div>
+		</div>
 			</div>
-			<div id="events_list">
-				event
+						<div id="events_list">
+							<button class="accordion">
+				<div class="flex">
+				<div class="event-desc">
+					<h3>25/9 Loppemarked</h3>
+					<p>Det er utroligt vigtigt med hurtig hjælp, da tusindvis af børn i Caribien lige nu står med et akut behov for vand, mad og beskyttelse efter orkanen Irmas voldsomme hærgen. Lions Katastrofehjælp har derfor straks doneret 250.000 kroner til UNICEFs indsats i regionen.</p>
+				</div>
+				</div>
+				
+				
+			</button>
+			<div class="accordion-content">
+				<div class="event_desc_wrapper">
+
+					<img style="float: right;" src="http://via.placeholder.com/200x200" alt="event description image">
+					<h5>lille overskrift</h5>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam sit eligendi repellendus et quibusdam ipsa dolorum doloribus, ipsam hic quae tempora, est expedita mollitia nihil a laboriosam, ipsum facilis saepe voluptas illo eaque! Doloremque ipsum, odit, fugit ipsam amet soluta dolor cupiditate sint, veritatis alias facilis quod accusamus in id!</p>
+				<h5>lille overskrift</h5> 
+				
+			
+				<p>Lorem ipsum dolor sit amet,s consectetur adipisicing elit. Totam sit eligendi repellendus et quibusdam ipsa dolorum doloribus, ipsam hic quae tempora, est expedita mollitia nihil a laboriosam, ipsum facilis saepe voluptas illo eaque! Doloremque ipsum, odit, fugit ipsam amet soluta dolor cupiditate sint, veritatis alias facilis quod accusamus in id!</p>
+		
+				<h5>Tidsplan</h5>
+				<div class="tidsplan">
+				<span>08:20 -  </span><p>Der er opvisning der og der </p>
+				<span>11:25 -  </span><p>Madboden åbner op for slag at mad og drikke huisk at medbring kontanter</p>
+				
+				</div>
+			
+				</div>
+		</div>
 			</div>
 		</div>
 
