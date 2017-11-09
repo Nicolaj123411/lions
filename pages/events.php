60784cfaf8e8f3815930edba82fffa57ab8bba0f
Bruger <bruger@brugers-mbp-3.home>
2017-11-09 15:34:38 +0100

    færdig styling

--- pages/events.php
+++ pages/events.php
@@ -8,13 +8,14 @@
 
 <section class="main_content_events">
 	<div class="event"> 
-		<div id="headline_box">Events</div>
+		<div id="headline_box">EVENTS</div>
+		<div id="next_event">
 			<button class="accordion">
 				<div class="flex">
 				<div class="event-desc">
 					<h1>NÆSTE EVENT</h1>
-					<h3>25/9 Loppemarked</h3>
-					<p>Det er utroligt vigtigt med hurtig hjælp, da tusindvis af børn i Caribien lige nu står med et akut behov for vand, mad og beskyttelse efter orkanen Irmas voldsomme hærgen. Lions Katastrofehjælp har derfor straks doneret 250.000 kroner til UNICEFs indsats i regionen.</p>
+					<h3>Jubilæumskoncert</h3>
+					<p>I september 2017 er det 30 år siden at Lions Fjends blev stiftet, som en lokal afdeling af den verdensomspændende humanitære organisation Lions Clubs International. Dette fejrer vi med en koncert med Prinsens Musikkorps og solist Mette Poulsen på Kongenshus Efterskole onsdag 15. november 2017, kl. 19-21:30. Under koncerten donerer vi midler til projekter i bl.a. lokalområdet.</p>
 				<ul class="info_icon">
 					<i class="fa fa-wheelchair"></i>
 					<i class="fa fa-coffee"></i>
@@ -24,50 +25,53 @@
 				</ul>
 				</div>
 				<div class="event-image">
-					<img src="http://via.placeholder.com/500x400" alt="Event picture">
+					<img id="events_billede" src="img/event_billeder/julekoncert.jpg" alt="Event picture">
 						<div class="img-overlay">
 							<p>Starter om</p>
 							<div>
-								<p>10</p>
-								<p>timer</p>
+								<h3>8</h3>
+								<h4>Dage</h4>
 							</div>
 							<div>
-								<p>20</p>
-								<p>min</p>
+								<h3>12</h3>
+								<h4>Timer</h4>
 							</div>
 						</div>
 					</div>
 				</div>
 				
-				
 			</button>
 			<div class="accordion-content">
 				<div class="event_desc_wrapper">
 
-					<img style="float: right;" src="http://via.placeholder.com/200x200" alt="event description image">
-					<h5>lille overskrift</h5>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam sit eligendi repellendus et quibusdam ipsa dolorum doloribus, ipsam hic quae tempora, est expedita mollitia nihil a laboriosam, ipsum facilis saepe voluptas illo eaque! Doloremque ipsum, odit, fugit ipsam amet soluta dolor cupiditate sint, veritatis alias facilis quod accusamus in id!</p>
-				<h5>lille overskrift</h5> 
+					
+					<h5>Jubilæumskoncert</h5>
+				<p>I september 2017 er det 30 år siden at Lions Fjends blev stiftet, som en lokal afdeling af den verdensomspændende humanitære organisation Lions Clubs International. Dette fejrer vi med en koncert med Prinsens Musikkorps og solist Mette Poulsen på Kongenshus Efterskole onsdag 15. november 2017, kl. 19-21:30.</p>
+
+				<h5>Tilmelding</h5> 
 				
 			
-				<p>Lorem ipsum dolor sit amet,s consectetur adipisicing elit. Totam sit eligendi repellendus et quibusdam ipsa dolorum doloribus, ipsam hic quae tempora, est expedita mollitia nihil a laboriosam, ipsum facilis saepe voluptas illo eaque! Doloremque ipsum, odit, fugit ipsam amet soluta dolor cupiditate sint, veritatis alias facilis quod accusamus in id!</p>
+				<p>Billetter til arrangementet (incl. kaffe og kage) koster kr. 50,- og kan købes og betales med Dankort.Desuden kan billetter reserveres hos Ejvind Jakobsen, tlf. 40411296.</p>
 		
 				<h5>Tidsplan</h5>
 				<div class="tidsplan">
-				<span>08:20 -  </span><p>Der er opvisning der og der </p>
-				<span>11:25 -  </span><p>Madboden åbner op for slag at mad og drikke huisk at medbring kontanter</p>
+				<span>19:00 -  </span><p>Velkomst og donationer samt servering af kaffe og kage</p>
+				<span>20:00 -  </span><p>Koncert med Prinsens Musikkorps og solist Mette Poulsen</p>
+				<span>21:00 -  </span><p>Afslutning</p>
 				
 				</div>
 			
 				</div>
+			</div>
 		</div>
 
 			<button class="accordion">
 				<div class="flex">
 				<div class="event-desc">
-					<h1>NÆSTE EVENT</h1>
-					<h3>25/9 Loppemarked</h3>
-					<p>Det er utroligt vigtigt med hurtig hjælp, da tusindvis af børn i Caribien lige nu står med et akut behov for vand, mad og beskyttelse efter orkanen Irmas voldsomme hærgen. Lions Katastrofehjælp har derfor straks doneret 250.000 kroner til UNICEFs indsats i regionen.</p>
+					<h1>Julemarkedet på Ulstrup Slot</h1>
+
+					<h3>2. December</h3>
+					<p>Snart åbner vi endnu engang for det årlige Julemarked på Ulstrup Slot, det er som sædvandlig den første hele weekend i december, dvs. den 2-3. december 2017. Husk at der er gratis entre og kun en lille p-afgift på 10kr</p>
 				<ul class="info_icon">
 					<i class="fa fa-wheelchair"></i>
 					<i class="fa fa-coffee"></i>
@@ -76,16 +80,16 @@
 				</ul>
 				</div>
 				<div class="event-image">
-					<img src="http://via.placeholder.com/500x400" alt="Event picture">
+					<img id="events_billede" src="img/event_billeder/julemarked.jpg" alt="Event picture">
 						<div class="img-overlay">
 							<p>Starter om</p>
 							<div>
-								<p>10</p>
-								<p>timer</p>
+								<h3>14</h3>
+								<h4>Dage</h4>
 							</div>
 							<div>
-								<p>20</p>
-								<p>min</p>
+								<h3>8</h3>
+								<h4>Timer</h4>
 							</div>
 						</div>
 					</div>
@@ -95,11 +99,12 @@
 
 
 			<div class="accordion-content">
-				<div class="event_desc_wrapper">jeg er afstand
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam sit eligendi repellendus et quibusdam ipsa dolorum doloribus, ipsam hic quae tempora, est expedita mollitia nihil a laboriosam, ipsum facilis saepe voluptas illo eaque! Doloremque ipsum, odit, fugit ipsam amet soluta dolor cupiditate sint, veritatis alias facilis quod accusamus in id!</p>
+				<div class="event_desc_wrapper">
+				<h5>Husk gratis entre</h5>
+				<p>Til Julemarkedet plejer vi at få besøg af 3.000 - 4.000 gæster, og for mange af vores gæster er det en tradition at besøge Julemarkedet og gøre et par juleindkøb. Mange har en dejlig familie oplevelse med børnebørn, med venner, naboer eller måske bare en lejlighed til at komme ud og få lidt frisk luft.</p>
 			
-				<p>Lorem ipsum dolor sit amet,s consectetur adipisicing elit. Totam sit eligendi repellendus et quibusdam ipsa dolorum doloribus, ipsam hic quae tempora, est expedita mollitia nihil a laboriosam, ipsum facilis saepe voluptas illo eaque! Doloremque ipsum, odit, fugit ipsam amet soluta dolor cupiditate sint, veritatis alias facilis quod accusamus in id!</p>
-				<p>Lorem ipsum dolor sit amet,s consectetur adipisicing elit. Totam sit eligendi repellendus et quibusdam ipsa dolorum doloribus, ipsam hic quae tempora, est expedita mollitia nihil a laboriosam, ipsum facilis saepe voluptas illo eaque! Doloremque ipsum, odit, fugit ipsam amet soluta dolor cupiditate sint, veritatis alias facilis quod accusamus in id!</p>
+				<h5>Åbent lørdag og søndag</h5> 
+				<p>Julemarkedet er åbent lørdag og søndag fra 10.00 - 16.00, så man har god tid til at nyde dagen på Ulstrup Slot - gøre en god handel, få noget lækkert til ganen og samtidig komme i julestemning i de mange hyggelige boder, der er opstillet i kælderen under hovedfløjen og i en af sidefløjene - det hele samlet omkring slotsgården.</p>
 
 
 				<button>luk event</button>
@@ -111,9 +116,9 @@
 			<button class="accordion">
 				<div class="flex">
 				<div class="event-desc">
-					<h1>NÆSTE EVENT</h1>
-					<h3>25/9 Loppemarked</h3>
-					<p>Det er utroligt vigtigt med hurtig hjælp, da tusindvis af børn i Caribien lige nu står med et akut behov for vand, mad og beskyttelse efter orkanen Irmas voldsomme hærgen. Lions Katastrofehjælp har derfor straks doneret 250.000 kroner til UNICEFs indsats i regionen.</p>
+					<h1>Bankospil i Ishøj</h1>
+					<h3>21. November</h3>
+					<p><p>Gennem efterhånden mange år Lions Club Ishøj har arrangeret bankospil for kommunens pensionister. I år er vi i Ishøj søndag eftermiddag samlet til bankospil i kantinen på Strandgårdskolen. Der bliver spillet om købmandskurve som hovedgevinster og en masse dejlige ting som række- og sidegevinster. Udover at spille banko er der også tid til hygge og drikke en kop kaffe med kringle eller en kold øl - alt sammen gratis. </p>
 				<ul class="info_icon">
 					<i class="fa fa-wheelchair"></i>
 					<i class="fa fa-coffee"></i>
@@ -122,16 +127,16 @@
 				</ul>
 				</div>
 				<div class="event-image">
-					<img src="http://via.placeholder.com/500x400" alt="Event picture">
+					<img id="events_billede" src="img/event_billeder/banko.jpg" alt="Event picture">
 						<div class="img-overlay">
 							<p>Starter om</p>
 							<div>
-								<p>10</p>
-								<p>timer</p>
+								<h3>9</h3>
+								<h4>Dage</h4>
 							</div>
 							<div>
-								<p>20</p>
-								<p>min</p>
+								<h3>18</h3>
+								<h4>Timer</h4>
 							</div>
 						</div>
 					</div>
@@ -139,12 +144,17 @@
 				
 			</button>
 			<div class="accordion-content">
-				<div class="event_desc_wrapper">jeg er afstand
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam sit eligendi repellendus et quibusdam ipsa dolorum doloribus, ipsam hic quae tempora, est expedita mollitia nihil a laboriosam, ipsum facilis saepe voluptas illo eaque! Doloremque ipsum, odit, fugit ipsam amet soluta dolor cupiditate sint, veritatis alias facilis quod accusamus in id!</p>
-			
-				<p>Lorem ipsum dolor sit amet,s consectetur adipisicing elit. Totam sit eligendi repellendus et quibusdam ipsa dolorum doloribus, ipsam hic quae tempora, est expedita mollitia nihil a laboriosam, ipsum facilis saepe voluptas illo eaque! Doloremque ipsum, odit, fugit ipsam amet soluta dolor cupiditate sint, veritatis alias facilis quod accusamus in id!</p>
-				<p>Lorem ipsum dolor sit amet,s consectetur adipisicing elit. Totam sit eligendi repellendus et quibusdam ipsa dolorum doloribus, ipsam hic quae tempora, est expedita mollitia nihil a laboriosam, ipsum facilis saepe voluptas illo eaque! Doloremque ipsum, odit, fugit ipsam amet soluta dolor cupiditate sint, veritatis alias facilis quod accusamus in id!</p>
+				<div class="event_desc_wrapper">
+				<h5>Husk! Det er gratis</h5>
+					
+				<p>Pensionist-Banko afholdes normalt den første søndag i november. Hvis du vil tilmeldes, så kan du sende en mail til ishoej@lions.dk.</p>
+				
 
+				<h5>Brilleindsamling</h5> 
+			
+				<p>I forbindelse med bankospillet bliver der indsamlet udtjente briller, som efter udmåling og rensning sendes til nye brugere i f.eks. Afrika. Efter alt at dømme bliver næste brilleindsamling i forbindelse med kommunal-valget den 21. november, hvor Lions Ishøj påtænker at opsætte indsamlingskasser ved de fem valgsteder i kommunen.</p>
+
+				<p>HUSK derfor de gamle briller, når du skal stemme! Skulle du gå hen at glemme brillerne – eller har du brevstemt, så er er der alligevel mulighed for at komme af med dem, så du skal blot ringe til 70 2635 70 og aftale et tidspunkt for afhentning. PÅ FORHÅND TAK.</p>
 
 				<button>luk event</button>
 				</div>
@@ -157,9 +167,9 @@
 			<button class="accordion">
 				<div class="flex">
 				<div class="event-desc">
-					<h1>NÆSTE EVENT</h1>
-					<h3>25/9 Loppemarked</h3>
-					<p>Det er utroligt vigtigt med hurtig hjælp, da tusindvis af børn i Caribien lige nu står med et akut behov for vand, mad og beskyttelse efter orkanen Irmas voldsomme hærgen. Lions Katastrofehjælp har derfor straks doneret 250.000 kroner til UNICEFs indsats i regionen.</p>
+					<h1>Kommunalvalgs Brilleindsamling</h1>
+					<h3>20. November</h3>
+					<p>Når du går til valgurnen ved kommunalvalget, kan du også donere dine brugte briller til Afrika. De 6 lions klubber i Randers kommune har opstillet indsamlingsbokse ved samtlige valgsteder i kommunen. Vores brilleindsamlinger plejer at være populære, fx indsamlede Lions klubberne ved EU-valget landet over mere end 16.000 par briller, og da vi ved, at valgdeltagelsen er noget højere ved kommunalvalg, håber vi på at kunne sætte en lille rekord!</p>
 				<ul class="info_icon">
 					<i class="fa fa-wheelchair"></i>
 					<i class="fa fa-coffee"></i>
@@ -168,16 +178,16 @@
 				</ul>
 				</div>
 				<div class="event-image">
-					<img src="http://via.placeholder.com/500x400" alt="Event picture">
+					<img id="events_billede" src="img/event_billeder/briller_2.jpg" alt="Event picture">
 						<div class="img-overlay">
 							<p>Starter om</p>
 							<div>
-								<p>10</p>
-								<p>timer</p>
+								<h3>4</h3>
+								<h4>Dage</h4>
 							</div>
 							<div>
-								<p>20</p>
-								<p>min</p>
+								<h3>21</h3>
+								<h4>Timer</h4>
 							</div>
 						</div>
 					</div>
@@ -185,11 +195,13 @@
 				
 			</button>
 			<div class="accordion-content">
-				<div class="event_desc_wrapper">jeg er afstand
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam sit eligendi repellendus et quibusdam ipsa dolorum doloribus, ipsam hic quae tempora, est expedita mollitia nihil a laboriosam, ipsum facilis saepe voluptas illo eaque! Doloremque ipsum, odit, fugit ipsam amet soluta dolor cupiditate sint, veritatis alias facilis quod accusamus in id!</p>
-			
-				<p>Lorem ipsum dolor sit amet,s consectetur adipisicing elit. Totam sit eligendi repellendus et quibusdam ipsa dolorum doloribus, ipsam hic quae tempora, est expedita mollitia nihil a laboriosam, ipsum facilis saepe voluptas illo eaque! Doloremque ipsum, odit, fugit ipsam amet soluta dolor cupiditate sint, veritatis alias facilis quod accusamus in id!</p>
-				<p>Lorem ipsum dolor sit amet,s consectetur adipisicing elit. Totam sit eligendi repellendus et quibusdam ipsa dolorum doloribus, ipsam hic quae tempora, est expedita mollitia nihil a laboriosam, ipsum facilis saepe voluptas illo eaque! Doloremque ipsum, odit, fugit ipsam amet soluta dolor cupiditate sint, veritatis alias facilis quod accusamus in id!</p>
+				<div class="event_desc_wrapper">
+			<p>Når Lions deler briller ud, oplever vi stor glæde. Pludselig kan den synshæmmede se tydeligt igen. Det øger ikke kun livskvaliteten, men betyder for mange at de nu også kan arbejde igen. Specielt Afrika og Latinamerika er på modtagerlisten.Over 30.000 par briller forlader hvert år vores opmålingssted for at blive fragtet ud i den store verden. Hovedparten af disse briller er blandt andet indsamlet gennem Lions.</p>
+
+			<p>Efter indsamling bliver brillerne sendty til opmåling hos en produktionsskole på Falster, hvor de opmåles, påsættes styrke, kontrolleres og pakkes i plastpose. Skolens elever sorterer briller, renser og måler dem op. De markeres med styrke og pakkes i kasser med 50 briller af samme styrke. Er brillerne kassable, tages glas ud, ligesom stel sorteres i metaller og plastik, hvorefter det hele sendes til genbrug. Når de defekte briller er sorteret fra, ender man op med ca. 30.000 par briller årligt, der får et nyt liv ude i verden.</p>
+
+			<p>På modtagerstedet sørger professionelle øjenlæger eller trænede frivillige Lions/Leos for at screene de synshæmmede, så brillerne kommer til de rette brugere. Kampen mod blindhed har altid været et kerneområde i Lions, f.eks. var Lions med i udviklingen af den hvide blindestok, uddannelse af førerhunde, etablering af øjenbanker og -hospitaler i 3. verden lande m.m. Derfor er det også naturligt at syn og brilleindsamling er i fokus her i Lions 100 års jubilæumsår.</p>
+
 
 
 				<button>luk event</button>
