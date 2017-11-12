5a29fecf2ff1ae7424faee931528d153d540fee5
Nicolaj123411 <nicolaj123411@gmail.com>
2017-11-12 17:52:05 +0100

    admin panel

--- admin/includes/menu.php
+++ admin/includes/menu.php
@@ -3,6 +3,6 @@
 			<ul>
 				<li><a href="index.php">Forside</a></li>
 				<li class="menu-active"><a href="?admin_page=events">Events</a></li>
-				<li><a href="#">Nyheder</a></li>
+				<li><a href="?admin_page=news">Nyheder</a></li>
 			</ul>
 		</nav>
